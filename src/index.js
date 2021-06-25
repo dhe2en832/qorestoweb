e4ba59130372eba425901ccbc705264827511379
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2021-06-25 10:42:59 +0700

    feat: add css baseline

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './styles/index.css';
 import '@fontsource/roboto';
 import App from './scripts/App';
@@ -8,6 +9,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
