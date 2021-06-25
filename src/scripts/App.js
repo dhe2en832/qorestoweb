616403267f335e2224242c137ba98c70b6df805e
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2021-06-25 10:42:46 +0700

    feat: add new router

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return <div>CSA Computer</div>;
+const Home = () => {
+  return (
+    <div>
+      <p>CSA Computer</p>
+    </div>
+  );
+};
+
+const NotFound = () => {
+  return (
+    <div>
+      <p>404. The Page Is Not Found</p>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <BrowserRouter basename="/researchCSA">
+      <Suspense fallback={'Please Wait...'}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/404" component={NotFound} />
+          <Redirect from="*" to="/404" />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
-
-export default App;
