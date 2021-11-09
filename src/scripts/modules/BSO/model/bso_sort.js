b15a55dea567926a78f61bcd262ec2cddf9d9d45
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2021-11-09 14:10:22 +0700

    feat: change index 2 to Kode Cust.

--- src/scripts/modules/BSO/model/bso_sort.js
+++ src/scripts/modules/BSO/model/bso_sort.js
@@ -1,6 +1,6 @@
 export const BSOSR = [
   { index: 1, title: 'Nomor SO' },
-  { index: 2, title: 'Customer' },
+  { index: 2, title: 'Kode Customer' },
   { index: 3, title: 'Tgl SO' },
   { index: 4, title: 'Sales Person' },
   { index: 5, title: 'PO Customer' },
