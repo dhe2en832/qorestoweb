300eb39f1ffd0173494e0afb27b04234bd7fd64d
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2021-11-19 14:09:28 +0700

    feat: add new hooks table dynamic

---
+++ src/scripts/hooks/useTableListsDynamic.js
@@ -0,0 +1,194 @@
+import { useState, useEffect } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+import AlertDialog from '../components/AlertDialog';
+// import ConfirmDialog from '../components/ConfirmDialog';
+import { typesError } from '../utils/types-error';
+import useSearch from './useSearch';
+import useRedirectToLogin from './useRedirectToLogin';
+
+export default function useTableLists({
+  dataSource,
+  headCells,
+  confPrimKey,
+  confName,
+  sortDataBy,
+}) {
+  const url = useRouteMatch().path;
+  const { redirectToLogin } = useRedirectToLogin();
+  const [columns, setColumns] = useState([]);
+  const [lists, setLists] = useState([]);
+  const [listCount, setListCount] = useState(null);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+  const [openKeySearchDlg, setOpenKeySearchDlg] = useState(false);
+  const [indexKey, setIndexKey] = useState(2);
+  const [openTextFilterDlg, setOpenTextFilterDlg] = useState(false);
+  const [textFilter, setTextFilter] = useState('');
+  const { search, handleSearch } = useSearch();
+  const [submitSearch, setSubmitSearch] = useState({ submitted: false, value: '' });
+  const searchLabel = sortDataBy
+    .filter((data) => data.index === indexKey)
+    .map((label) => label.title)[0];
+  const [loading, setLoading] = useState(false);
+  const [dense, setDense] = useState(false);
+  const [useBRWDEF, setUseBRWDEF] = useState(false);
+
+  const handleSubmitSearch = () => {
+    setSubmitSearch({
+      submitted: true,
+      value: search,
+    });
+    setListCount(null);
+    setOffset(0);
+    setPage(0);
+  };
+
+  const handleTextFilter = (value) => {
+    setTextFilter(value);
+    setListCount(null);
+    setOffset(0);
+    setPage(0);
+  };
+
+  //#region handleDelete
+  // const handleDelete = (event, id) => {
+  //   event.preventDefault();
+  //   ConfirmDialog(
+  //     'Apakah anda yakin menghapus?',
+  //     `Data ${initName} tidak akan bisa dikembalikan`,
+  //     'Ya, Hapus',
+  //     async () => {
+  //       try {
+  //         const deleteDatas = await dataSource.delete(id);
+  //         AlertDialog(
+  //           'success',
+  //           `Berhasil Menghapus Data ${initName}`,
+  //           <p>
+  //             Data {initName} dengan ID/Kode: "{id}" telah terhapus.
+  //           </p>,
+  //           () => {
+  //             const numericHead = headCells.filter((headCell) => headCell.numeric === true);
+  //             if (numericHead.length > 0) {
+  //               const undeletedDataWithNumeric = deleteDatas.onsuccess.data.map((getDataNum) => {
+  //                 numericHead.map(
+  //                   (numHead) => (getDataNum[numHead.id] = parseFloat(getDataNum[numHead.id]))
+  //                 );
+  //                 return getDataNum;
+  //               });
+  //               setLists(undeletedDataWithNumeric);
+  //             } else {
+  //               setLists(deleteDatas.onsuccess.data);
+  //             }
+  //           }
+  //         );
+  //       } catch (error) {}
+  //     }
+  //   );
+  // };
+  //#endregion
+
+  useEffect(() => {
+    let isActive = true;
+    const getLists = async () => {
+      try {
+        setLoading(true);
+        const listFieldsMap = headCells.map((head) => head.id);
+        const dataOptions = {
+          offset,
+          limit: listCount === null ? 0 : limit,
+          usebrwdef: useBRWDEF,
+          listfields: listFieldsMap,
+          query: {
+            keysearch: {
+              index: indexKey,
+              search: submitSearch.value,
+            },
+            textfilter: {
+              search: textFilter,
+            },
+          },
+        };
+        const getDatas = await dataSource.getList(dataOptions);
+        if (getDatas.result === true) {
+          if (listCount === null) setListCount(getDatas.metadata.total);
+          else if (getDatas.columns) {
+            setColumns(getDatas.columns)
+            setLists(getDatas.data)
+          }
+          else {
+            setUseBRWDEF(false);
+            setLists(getDatas.data);
+          }
+        } else if (getDatas.result === false) {
+          throw getDatas.onfail.cerror;
+        } else throw getDatas.message;
+      } catch (error) {
+        switch (error) {
+          case typesError.SECRET_INVALID.msg:
+            AlertDialog('error', ...typesError.SECRET_INVALID.res, redirectToLogin);
+            break;
+          case typesError.SESSION_INVALID.msg:
+            AlertDialog('error', ...typesError.SESSION_INVALID.res, redirectToLogin);
+            break;
+          case typesError.SESSION_LOCKED.msg:
+            typesError.SESSION_LOCKED.func();
+            break;
+          case typesError.SESSION_TIMEOUT.msg:
+            AlertDialog('error', ...typesError.SESSION_TIMEOUT.res, redirectToLogin);
+            break;
+          case typesError.FETCH.msg:
+            AlertDialog('error', 'Salah', typesError.FETCH.res);
+            break;
+          default:
+            AlertDialog('error', 'Salah', error);
+            break;
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (isActive === true) getLists();
+    return () => isActive = false;
+  }, [
+    dataSource,
+    headCells,
+    listCount,
+    useBRWDEF,
+    indexKey,
+    confName,
+    offset,
+    limit,
+    submitSearch,
+    setSubmitSearch,
+    textFilter,
+    redirectToLogin,
+  ]);
+
+  return {
+    url,
+    loading,
+    searchLabel,
+    handleSearch,
+    handleSubmitSearch,
+    openKeySearchDlg,
+    setOpenKeySearchDlg,
+    indexKey,
+    setIndexKey,
+    openTextFilterDlg,
+    setOpenTextFilterDlg,
+    handleTextFilter,
+    columns,
+    lists,
+    listCount,
+    setListCount,
+    setOffset,
+    limit,
+    setLimit,
+    page,
+    setPage,
+    dense,
+    setDense,
+    useBRWDEF,
+  };
+}
---
+++ src/scripts/hooks/useTableListsLookupDynamic.js
@@ -0,0 +1,174 @@
+import { useState, useEffect } from 'react';
+import AlertDialogNested from '../components/AlertDialogNested';
+import { typesError } from '../utils/types-error';
+import useSearch from './useSearch';
+// import usePopupFormAdd from './usePopupFormAdd';
+
+export default function useTableListsLookupDynamic({
+  dataSource,
+  headCells,
+  confPrimKey,
+  confSecKey,
+  confName,
+  sortDataBy,
+  isLoginPopup,
+  handleOpenLoginPopup,
+}) {
+  const idElemLookup = `Lookup${confName.replace(/\/| /g, '')}`;
+  const [columns, setColumns] = useState([]);
+  const [lists, setLists] = useState([]);
+  const [listCount, setListCount] = useState(null);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+  const [openKeySearchDlg, setOpenKeySearchDlg] = useState(false);
+  const [indexKey, setIndexKey] = useState(2);
+  const [openTextFilterDlg, setOpenTextFilterDlg] = useState(false);
+  const [textFilter, setTextFilter] = useState('');
+  const { search, handleSearch } = useSearch();
+  const [submitSearch, setSubmitSearch] = useState({ submitted: false, value: '' });
+  const searchLabel = sortDataBy
+    .filter((data) => data.index === indexKey)
+    .map((label) => label.title)[0];
+  const [loading, setLoading] = useState(false);
+  const [dense, setDense] = useState(false);
+  const [useBRWDEF, setUseBRWDEF] = useState(true);
+
+  const handleSubmitSearch = () => {
+    setSubmitSearch({
+      submitted: true,
+      value: search,
+    });
+    setListCount(null);
+    setOffset(0);
+    setPage(0);
+  };
+
+  const handleTextFilter = (value) => {
+    setTextFilter(value);
+    setListCount(null);
+    setOffset(0);
+    setPage(0);
+  };
+
+  useEffect(() => {
+    let isActive = true;
+    const getList = async () => {
+      try {
+        setLoading(true);
+        const listFieldsMap = headCells.map((head) => head.id);
+        const dataOptions = {
+          offset,
+          limit: listCount === null ? 0 : limit,
+          usebrwdef: useBRWDEF,
+          listfields: listFieldsMap,
+          query: {
+            keysearch: {
+              index: indexKey,
+              search: submitSearch.value,
+            },
+            textfilter: {
+              search: textFilter,
+            },
+          },
+        };
+        const getDatas = await dataSource.getList(dataOptions);
+        if (getDatas.result === true) {
+          if (listCount === null) setListCount(getDatas.metadata.total);
+          else if (getDatas.columns) {
+            setColumns(getDatas.columns)
+            setLists(getDatas.data)
+          }
+          else {
+            setUseBRWDEF(false);
+            setLists(getDatas.data);
+          }
+        } else if (getDatas.result === false) {
+          throw getDatas.onfail.cerror;
+        } else throw getDatas.message;
+      } catch (error) {
+        switch (error) {
+          case typesError.SECRET_INVALID.msg:
+            AlertDialogNested(
+              idElemLookup,
+              'error',
+              ...typesError.SECRET_INVALID.res,
+              handleOpenLoginPopup
+            );
+            break;
+          case typesError.SESSION_INVALID.msg:
+            AlertDialogNested(
+              idElemLookup,
+              'error',
+              ...typesError.SESSION_INVALID.res,
+              handleOpenLoginPopup
+            );
+            break;
+          case typesError.SESSION_LOCKED.msg:
+            typesError.SESSION_LOCKED.func();
+            break;
+          case typesError.SESSION_TIMEOUT.msg:
+            AlertDialogNested(
+              idElemLookup,
+              'error',
+              ...typesError.SESSION_TIMEOUT.res,
+              handleOpenLoginPopup
+            );
+            break;
+          case typesError.FETCH.msg:
+            AlertDialogNested(idElemLookup, 'error', 'Salah', typesError.FETCH.res);
+            break;
+          default:
+            AlertDialogNested(idElemLookup, 'error', 'Salah', error);
+            break;
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    // if (showPopupFormAdd === false && isLoginPopup === false) getList();
+    if (isLoginPopup === false && isActive === true) getList();
+    return () => isActive = false;
+  }, [
+    dataSource,
+    headCells,
+    listCount,
+    useBRWDEF,
+    indexKey,
+    confName,
+    idElemLookup,
+    offset,
+    limit,
+    submitSearch,
+    textFilter,
+    isLoginPopup,
+    handleOpenLoginPopup,
+  ]);
+
+  return {
+    idElemLookup,
+    loading,
+    searchLabel,
+    handleSearch,
+    handleSubmitSearch,
+    openKeySearchDlg,
+    setOpenKeySearchDlg,
+    indexKey,
+    setIndexKey,
+    openTextFilterDlg,
+    setOpenTextFilterDlg,
+    handleTextFilter,
+    columns,
+    lists,
+    listCount,
+    setListCount,
+    setOffset,
+    limit,
+    setLimit,
+    page,
+    setPage,
+    dense,
+    setDense,
+    useBRWDEF,
+  };
+}
