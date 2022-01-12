01100ca1892032330e0fcccf09680f5c15386d5a
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2022-01-12 14:19:59 +0700

    feat: change style of slogan

--- src/scripts/components/CompanySlogan.jsx
+++ src/scripts/components/CompanySlogan.jsx
@@ -6,11 +6,12 @@ import useResponsive from '../hooks/useResponsive';
 export default function CompanySlogan() {
   const { mdUp } = useResponsive();
   return (
-    <Box mt={2} p={2} bgcolor="black">
+    <Box>
       <Typography
         component="h2"
-        variant={mdUp ? 'h5' : 'body1'}
-        sx={{ color: '#fff' }}
+        variant={mdUp ? 'h6' : 'body2'}
+        sx={{ color: '#fff', bgcolor: '#000', width: mdUp ? '60%' : '100%', m: '0 auto', p: 1 }}
+        fontWeight={mdUp ? 500 : 400}
         align="center"
       >
         Integrated Business Applications and Quality Services
