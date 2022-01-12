4ec0745c47ad449ebdbad545057cd8b6bd553e29
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2022-01-12 14:39:48 +0700

    feat: add new system logout

--- src/scripts/modules/HOME/index.js
+++ src/scripts/modules/HOME/index.js
@@ -5,6 +5,7 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import MenuIcon from '@mui/icons-material/BlurCircularOutlined';
+import LogoutIcon from '@mui/icons-material/Logout';
 import CompanyName from '../../components/CompanyName';
 import CompanySlogan from '../../components/CompanySlogan';
 import ModuleContext from '../../contexts/ModuleContext';
@@ -21,6 +22,8 @@ export default function Home() {
   const styles = {
     root: {
       padding: '24px',
+      backgroundColor: '#eee',
+      borderRadius: 0
     },
     linkText: {
       textDecoration: 'none',
@@ -40,16 +43,16 @@ export default function Home() {
 
   return (
     <Container maxWidth="lg">
-      <Paper sx={styles.root}>
+      <Paper sx={styles.root} elevation={0}>
         <CompanyName />
         <CompanySlogan />
       </Paper>
-      <Grid container mt={2} mb={5} spacing={2} alignItems="flex-start">
+      <Grid container mt={0} mb={5} spacing={2} alignItems="flex-start">
         {ModuleContext.map(
           ({ name, menu }, index) =>
             !isEmptyModules(ModuleContext, name) && (
               <Grid item container key={('hm_menu_items_' + name + index).toString()}>
-                <Typography component="h1" variant="h5" color="secondary">
+                <Typography component="h1" variant="h6" color="secondary">
                   {name}
                 </Typography>
                 <Grid item container spacing={1} mt={0.1}>
@@ -80,6 +83,28 @@ export default function Home() {
               </Grid>
             )
         )}
+        <Grid item container>
+          <Typography component="h1" variant="h6" color="secondary">
+            System
+          </Typography>
+          <Grid item container spacing={1} mt={0.1}>
+            <Grid
+              item
+              xs={12}
+              md={4}
+              sx={styles.linkText}
+            >
+              <Paper elevation={2} sx={{ p: 2, cursor: 'pointer' }} onClick={() => auth.signout()}>
+                <Grid container justifyContent="space-between">
+                  <LogoutIcon />
+                  <Typography component="h2" variant="subtitle1" ml={1} color="error">
+                    Logout
+                  </Typography>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
     </Container>
   );
