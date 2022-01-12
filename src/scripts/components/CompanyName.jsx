77c1614d0ef9141e4dd9b4ecc2b0262aad8a2cb8
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2022-01-12 14:19:48 +0700

    feat: change the size of typography

--- src/scripts/components/CompanyName.jsx
+++ src/scripts/components/CompanyName.jsx
@@ -5,11 +5,11 @@ import useResponsive from '../hooks/useResponsive';
 export default function CompanyName() {
   const { mdUp } = useResponsive();
   return (
-    <Typography component="h1" variant={mdUp ? 'h3' : 'h4'} sx={{ fontWeight: 600 }} align="center">
+    <Typography component="h1" variant={mdUp ? 'h4' : 'h6'} sx={{ fontWeight: 600 }} align="center">
       Citra Sabda Abadi{' '}
       <Typography
         component="span"
-        variant={mdUp ? 'h4' : 'h6'}
+        variant={mdUp ? 'h6' : 'body1'}
         sx={{ color: '#513474', fontStyle: 'italic' }}
       >
         {' '}
