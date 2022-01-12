b425f31f46531f007ebfa6bf066105d97da95fe3
Moch Ridwan Taufik Alamsyah <ridwan@csacomputer.com>
2022-01-12 14:22:06 +0700

    feat: add Logo and fix error

--- src/scripts/modules/NOTFOUND/index.js
+++ src/scripts/modules/NOTFOUND/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-// import { ReactComponent as Logo } from '../../../images/404-illustration.svg';
+import { ReactComponent as Logo } from '../../../images/404-illustration.svg';
 
 export default function NotFound() {
   return (
     <Container maxWidth="lg">
       <Box alignItems="center">
-        {/* <img
-          src={}
-          alt="404 The Page Was Not Found"
-          style={{ width: '100%', maxHeight: '409.6px' }}
-        /> */}
-        <Typography textAlign="center" variant="" component="h1">
+        <Logo style={{ width: '100%', maxHeight: '500px', marginTop: 30 }} />
+        <Typography textAlign="center" variant="h6" fontWeight={600} component="h1" position="relative" sx={{ top: -80 }} color="secondary">
           Halaman tersebut tidak ditemukan
         </Typography>
       </Box>
